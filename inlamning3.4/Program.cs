4b9da7efc53fbdc39b83d425e1ab2eb65fa0b730
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-22 16:48:02 +0200

    Add project files.

---
+++ inlamning3.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace inlamning3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är din låt?");
+            string answer = Console.ReadLine();
+            int space = answer.IndexOf(" ");
+            int min = int.Parse(answer[..space]);
+            int sec = int.Parse(answer[(space +1)..]);
+            int totalLength = (min * 60) + sec;
+
+            if (totalLength >= 165 && totalLength <= 260)
+                Console.WriteLine("Låten får spelas");
+
+            else
+                Console.WriteLine("Låten får inte spelas");
+
+
+
+        }
+
+    }
+
+}
